a373c8ee55489c6061520dc73d6b33034450b8e8
pphongsakorn-cloud <p.phongsakorn@gmail.com>
2026-08-20 08:08:34 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,27 @@
-const CACHE_NAME = "the-rise-suites-v1";
+const CACHE_NAME = "the-rise-suites-v2";
 
 self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
